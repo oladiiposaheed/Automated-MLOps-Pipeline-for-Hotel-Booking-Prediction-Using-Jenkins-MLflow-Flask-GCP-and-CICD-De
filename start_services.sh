358c12e213b33de3abc3907b53f724d3e4f5b721
Saheed <oladiiposaheed@gmail.com>
2025-10-11 02:02:22 +0100

    commit 9

--- start_services.sh
+++ start_services.sh
@@ -1,6 +1,14 @@
 # Start Flask app in background
 
-python app.py &
+# python app.py &
 
-# Start Django app in foreground (this keeps the container running)
-python hotel_reservation/hotelpredictor/manage.py runserver 0.0.0.0:8000
+# # Start Django app in foreground (this keeps the container running)
+# python hotel_reservation/hotelpredictor/manage.py runserver 0.0.0.0:8000
+
+
+# Cloud Run
+PORT=${PORT:-8080}
+
+echo "Starting Django application on port: $PORT"
+
+exec python hotel_reservation/hotelpredictor/manage.py runserver 0.0.0.0:$PORT
